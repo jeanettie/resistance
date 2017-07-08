7801447b8aa14a7c9e56a013ff684acaf1f558c1
jeanettie <jeanettienicole@gmail.com>
2017-07-08 16:54:52 -0400

    updating styles.css file

--- resistance/App_Start/BundleConfig.cs
+++ resistance/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace resistance
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/site.css",
                       "~/Content/styles.css"));
         }
     }
