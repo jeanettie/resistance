762ccc925ee5c9844b6bb9eba26772d36a5f5cd1
jeanettie <jeanettienicole@gmail.com>
2017-07-11 22:17:01 -0400

    added post class in models

---
+++ resistance/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace resistance.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Body { get; set; }
+        public string Meta { get; set; }
+        public string UrlSeo { get; set; }
+        public bool Published { get; set; }
+        public DateTime PostedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+
+
+    }
+}
